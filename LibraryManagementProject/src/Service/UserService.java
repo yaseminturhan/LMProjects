da27ca79924190525bc4a4edbd2dd0fbdc3f0f7a
Yasemin Turhan <47610298+yaseminturhan@users.noreply.github.com>
2020-05-19 12:14:16 +0300

    Add files via upload

--- LibraryManagementProject/src/Service/UserService.java
+++ LibraryManagementProject/src/Service/UserService.java
@@ -5,55 +5,158 @@
  */
 package Service;
 
+
 import Interface.UserRepository;
-import Models.User;
+import Interface.BookRepository;
+import Models.UserModel;
 import java.util.List;
+import ClassImplemantation.FileProcess;
+import ClassImplemantation.Global;
+import ClassImplemantation.User;
+import java.util.ArrayList;
+import java.util.Random;
+import ClassImplemantation.TakeInformation;
+import Models.Book;
+import java.util.Date;
 
 /**
  *
  * @author HP
  */
-public class UserService implements UserRepository{
+
+/**
+ *
+ * @author HP
+ */
+public class UserService extends FileProcess implements UserRepository{
+    
+    private static final String USERS_FILE = "users.txt";
+
+
+     public UserModel getByDepartment(String department) {
+        String line = super.readLineById(USERS_FILE, department);
+        return toUser(line);
+    }
+    
 
     @Override
-    public List<User> getByDepartment(String department) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void save(UserModel t) {
+        
     }
 
     @Override
-    public void save(User t) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void save(UserModel t, String id) {
+                t.setId(Integer.parseInt(id));
+		super.writeFile(USERS_FILE, t.toString());
     }
 
     @Override
-    public void save(User t, String id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void update(String id, User t) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void update(String id, UserModel t) {
+        String temp =id;
+	delete(id);
+	this.save(t,temp); 
     }
 
     @Override
     public void delete(String id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        super.deleteLine(USERS_FILE, id); 
+    }
+    
+    private List<Book> allToBook(List<String> lines){
+        List<Book> books = new ArrayList<>();
+        for(String line : lines){
+            String[] datas = line.split("_");
+            Book book = new Book(
+                    datas[0],
+                    datas[1],
+                    datas[2],
+                    datas[3],
+                    datas[4],
+                    datas[5]
+            );        
+            books.add(book);
+        }
+        return books;
     }
 
     @Override
-    public List<User> getAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public List<UserModel> getAll() {
+        List<String> lines = super.readLines(USERS_FILE);
+        return allToUser(lines);
+    }
+    
+    private List<UserModel> allToUser(List<String> lines){
+        
+        List<UserModel> users = new ArrayList<>();
+        for(String line: lines){
+            String[] datas = line.split("_");
+            List<String> userBooks = super.readLines(datas[3]+".txt");
+            UserModel user = new UserModel(
+                Integer.valueOf(datas[0]),
+                datas[1],
+                datas[2],
+                datas[3],
+                datas[4],
+                datas[5],
+                Boolean.valueOf(datas[6]),
+                allToBook(userBooks)
+            );
+        }
+        
+        return users;
+       
+       
+    }
+    
+    private UserModel toUser(String line){
+        //datas[3] kullanıcı adı olan dosyayı bulmak için
+        String[] datas = line.split("_");
+        List<String> userBooks = super.readLines(datas[3]+".txt");
+        
+        return new UserModel(
+                Integer.valueOf(datas[0]),
+                datas[1],
+                datas[2],
+                datas[3],
+                datas[4],
+                datas[5],
+                Boolean.valueOf(datas[6]),
+                allToBook(userBooks)
+        );
+        
+    }
+    
+    private Book toBook(String line){
+        String[] datas = line.split("_");
+        return new Book(
+                datas[0],
+                datas[1],
+                datas[2],
+                datas[3],
+                datas[4],
+                datas[5]
+        ); 
+        
+    }
+    
+
+
+    @Override
+    public UserModel getById(String id) {
+        String line = super.readLineById(USERS_FILE, id);
+        return toUser(line);
     }
 
     @Override
-    public User getById(String id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public UserModel getByUsername(String username) {
+        String line = super.readLineById(USERS_FILE, username);
+        return toUser(line);
     }
-
-    @Override
-    public User getByUsername(String username) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    
+    
+ 
+    
+    
 
     
 }
