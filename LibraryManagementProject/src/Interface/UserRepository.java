9acb1a46066331245c70a48c80fbba2175ef3268
Yasemin Turhan <47610298+yaseminturhan@users.noreply.github.com>
2020-05-21 02:05:53 +0300

    Add files via upload

--- LibraryManagementProject/src/Interface/UserRepository.java
+++ LibraryManagementProject/src/Interface/UserRepository.java
@@ -14,6 +14,9 @@ import java.util.List;
  */
 public interface UserRepository extends CrudRepository<UserModel>{
     
-    UserModel getByDepartment(String department);
-    UserModel getByUsername(String username);
+    //UserModel getByDepartment(String department);
+   // UserModel getByUsername(String username);
+    
+    
+    
 }
