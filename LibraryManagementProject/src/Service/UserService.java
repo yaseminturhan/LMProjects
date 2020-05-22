38158d0a2e70b26bd93afb8b0a836a0eaf3860e6
Seyit Ali Yaman <36454894+seyitaliyaman@users.noreply.github.com>
2020-05-22 15:50:07 +0300

    fix

--- LibraryManagementProject/src/Service/UserService.java
+++ LibraryManagementProject/src/Service/UserService.java
@@ -7,24 +7,12 @@ package Service;
 
 
 import Interface.UserRepository;
-import Interface.BookRepository;
 import Models.UserModel;
 import java.util.List;
 import ClassImplemantation.FileProcess;
-import ClassImplemantation.Global;
-import ClassImplemantation.User;
 import java.util.ArrayList;
-import java.util.Random;
-import ClassImplemantation.TakeInformation;
 import Models.Book;
-import java.util.Date;
-import Service.UserService;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import static java.nio.file.Files.lines;
+
 /**
  *
  * @author HP
@@ -37,23 +25,20 @@ import static java.nio.file.Files.lines;
 public class UserService extends FileProcess implements UserRepository{
     
     private static final String USERS_FILE = "userdeneme.txt";
-
-
-     /*public UserModel getByDepartment(String department) {
-        String line = super.readLineById(USERS_FILE, department);
-        return toUser(line);
-    }
-    */
+    private BookService bookService = new BookService();
 
     @Override
     public void save(UserModel t) {
-        String lines = t.getId() + "___" + t.getName() + "___" + t.getSurname()+ "___" + t.getUsername()+ "___" + t.getPassword()+ "___" + t.getDepartment()+ "___" + t.getIsAdmin();
-        super.writeFile(USERS_FILE,lines);
+        int id = super.getLastId(USERS_FILE);
+        t.setId(String.valueOf(id));
+        super.writeFile(USERS_FILE,t.toString());
+        super.writeFile(t.getUsername()+".txt", "");
     }
 
     @Override
     public void save(UserModel t, String id) {
-                
+        t.setId(id);
+        super.writeFile(USERS_FILE,t.toString());
     }
     
     
@@ -71,37 +56,10 @@ public class UserService extends FileProcess implements UserRepository{
         save(userupdate);
     }
     
-    public ArrayList<String> readLines(String fileName) {
-		ArrayList<String> lines = new ArrayList<>();
-		try {
-			BufferedReader bufferedReader = new BufferedReader(this.getFileReader(USERS_FILE));
-			String line;
-			while((line = bufferedReader.readLine()) != null) {
-				lines.add(line);
-			}
-			bufferedReader.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return lines;
-	}
-    
-    	private FileReader getFileReader(String fileName) throws IOException {
-		File file = new File(fileName);
-		if(!file.exists()) {
-			file.createNewFile();
-		}
-		return new FileReader(file.getAbsoluteFile());
-	}
-        
-        
     
     public void delete(String id) {
         super.deleteLine(USERS_FILE, id); 
     }
-    
-    
-    
 
     @Override
     public List<UserModel> getAll() {
@@ -112,47 +70,45 @@ public class UserService extends FileProcess implements UserRepository{
     private List<UserModel> allToUser(List<String> lines){
         
         List<UserModel> users = new ArrayList<>();
-        for(String line: lines){
-            String[] datas = line.split("_");
-           
-            UserModel user = new UserModel(
-                Integer.valueOf(datas[0]),
-                datas[1],
-                datas[2],
-                datas[3],
-                datas[4],
-                datas[5],
-                Boolean.valueOf(datas[6])
-                
-            );
-            users.add(user);
-        }
-        
-        return users;
-       
-       
+        List<Book> books = new ArrayList<>();
+        System.out.println(lines.size());
+        if(lines.size() == 0){
+            return users;
+        }else{
+            for(String line: lines){
+                String[] datas = line.split("___");
+                UserModel user = new UserModel(
+                    datas[0],
+                    datas[1],
+                    datas[2],
+                    datas[3],
+                    datas[4],
+                    datas[5],
+                    Boolean.valueOf(datas[6])
+                );
+                users.add(user);
+            }
+            return users;
+        } 
     }
     
     private UserModel toUser(String line){
         //datas[3] kullanıcı adı olan dosyayı bulmak için
-        String[] datas = line.split("_");
+        String[] datas = line.split("___");
         List<String> userBooks = super.readLines(datas[3]+".txt");
+        List<Book> books = new ArrayList<>();
         
         return new UserModel(
-                Integer.valueOf(datas[0]),
+                datas[0],
                 datas[1],
                 datas[2],
                 datas[3],
                 datas[4],
                 datas[5],
-                Boolean.valueOf(datas[6])
-                
+                Boolean.valueOf(datas[6])  
         );
         
     }
-    
-   
-
 
     @Override
     public UserModel getById(String id) {
@@ -160,18 +116,28 @@ public class UserService extends FileProcess implements UserRepository{
         return toUser(line);
     }
 
-    /*@Override
+    @Override
     public UserModel getByUsername(String username) {
         String line = super.readLineById(USERS_FILE, username);
         return toUser(line);
-    }*/
+    }
+    
+    @Override
+    public void addBookUser(String username,Book book) {
+        String fileName = username+".txt";
+        super.writeFile(fileName, book.toString());
+    }
 
-    
-    
-    
- 
-    
-    
+    @Override
+    public List<Book> getUserBooks(String username) {
+        return bookService.allToBook(super.readLines(username+".txt"));
+    }
 
+    @Override
+    public Book removeBookUser(String username, String bookId) {
+        Book book = bookService.getById(bookId);
+        super.deleteLine(username+".txt", bookId);
+        return book;
+    }  
     
 }
